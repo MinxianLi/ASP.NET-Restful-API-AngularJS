5d035fdabe584b3d3feda390313eab40e6ea1487
MinxianLi <lmx423@gmail.com>
2016-12-31 16:22:33 -0600

    Add working solution

--- AngularJSSample/AngularJSSample/Properties/AssemblyInfo.cs
+++ AngularJSSample/AngularJSSample/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AngularJSSample")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("97930599-4cdd-48a2-ba96-00cf667538b2")]
+[assembly: Guid("3a0fcbfd-e7b6-4fb2-825f-5f27e2ab8828")]
 
 // Version information for an assembly consists of the following four values:
 //
